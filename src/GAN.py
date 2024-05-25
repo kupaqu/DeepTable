6b6c2d2cf91492aec1993caf2ed4ad2d4fd2ba84
Buda Vampilov <fccfcbyjhtk@gmail.com>
2024-05-25 09:53:08 +0900

    small fix generator forward method arguments in GAN.py

--- src/GAN.py
+++ src/GAN.py
@@ -23,7 +23,7 @@ class GAN:
     
     def g_forward(self, meta: torch.Tensor):
         batch_size = meta.shape[0]
-        meta = meta.view(batch_size, len(meta), 1, 1)
+        meta = meta.view(batch_size, -1, 1, 1)
         z = torch.randn(batch_size, 32, 1, 1, device=self._device)
 
         return self.g(z, meta).view_as(-1, 16, 128)
