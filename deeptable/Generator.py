82fb45b2af4831eaa056b7a807de8bd0cec48b9c
Buda Vampilov <fccfcbyjhtk@gmail.com>
2024-12-12 05:02:05 +0900

    changed architecture of generator

--- deeptable/Generator.py
+++ deeptable/Generator.py
@@ -10,34 +10,34 @@ class Generator(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(64, 128, kernel_size=(4, 7)),
+            nn.ConvTranspose2d(64, 128, kernel_size=(4, 7), stride=(1, 2), padding=(0, 1)),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(128, 64, kernel_size=(4, 7)),
+            nn.ConvTranspose2d(128, 64, kernel_size=(4, 7), stride=(1, 2), padding=(0, 1)),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(64, 32, kernel_size=(4, 7)),
+            nn.ConvTranspose2d(64, 32, kernel_size=(4, 7), stride=(1, 2), padding=(0, 1)),
             nn.BatchNorm2d(32),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(32, 1, kernel_size=(4, 8))
+            nn.ConvTranspose2d(32, 1, kernel_size=(4, 8), stride=(1, 2), padding=(0, 1))
         )
 
-    def forward(self, x: torch.Tensor, meta: torch.Tensor, n_classes: torch.Tensor):
-        device = x.get_device()
+    def forward(self, z: torch.Tensor, meta: torch.Tensor, n_classes: torch.Tensor):
+        device = z.get_device()
 
         to_concat = []
-        input_ = torch.cat((x, meta), 1)
+        input_ = torch.cat((z, meta), 1)
         for i in range(n_classes):
             pos = torch.ones((input_.shape[0], 1, 1, 1), device=device) * torch.tensor(i, device=device)
             input_with_pos = torch.cat((input_, pos), 1)
             
             output = self.upconv(input_with_pos)
-            print(output.shape)
+            # print(output.shape)
             to_concat.append(output)
 
         concat = torch.cat(to_concat, 3)
-
+        print(concat.shape)
         return concat
