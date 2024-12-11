970981fc0af0dfc649671ddbc37cafff7de984ec
Buda Vampilov <fccfcbyjhtk@gmail.com>
2024-12-12 04:35:08 +0900

    changed architecture of generator

--- deeptable/Generator.py
+++ deeptable/Generator.py
@@ -10,19 +10,19 @@ class Generator(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(64, 128, kernel_size=(4, 7)),
+            nn.ConvTranspose2d(64, 128, kernel_size=(4, 7), stride=(1, 2), padding=(0, 2)),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(128, 64, kernel_size=(4, 7)),
+            nn.ConvTranspose2d(128, 64, kernel_size=(4, 7), stride=(1, 2), padding=(0, 2)),
             nn.BatchNorm2d(64),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(64, 32, kernel_size=(4, 7)),
+            nn.ConvTranspose2d(64, 32, kernel_size=(4, 7), stride=(1, 2), padding=(0, 2)),
             nn.BatchNorm2d(32),
             nn.ReLU(True),
 
-            nn.ConvTranspose2d(32, 1, kernel_size=(4, 8))
+            nn.ConvTranspose2d(32, 1, kernel_size=(4, 8), padding=(0, 2))
         )
 
     def forward(self, x: torch.Tensor, meta: torch.Tensor, n_classes: torch.Tensor):
